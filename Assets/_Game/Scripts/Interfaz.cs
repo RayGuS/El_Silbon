b087cea8788a945152fa9951367cb728a524e9af
natix0128 <127872082+natix0128@users.noreply.github.com>
2023-04-27 17:46:15 -0500

    InicioScene (UI)

--- Assets/_Game/Scripts/Interfaz.cs
+++ Assets/_Game/Scripts/Interfaz.cs
@@ -3,25 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Interfaz : MonoBehaviour
 {
-    public GameObject victoria;
-    public GameObject gameOver;
+    //public GameObject victoria;
+    //public GameObject gameOver;
+    public GameObject configuracion;
+    public GameObject creditos;
+    public GameObject inicio;
+    public GameObject salir;
 
-    public BarraVida vida;
+    //public BarraVida vida;
 
-    public void Victoria()
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Natalia");
+    }
+    public void Configuracion()
+    {
+        configuracion.SetActive(true);
+        inicio.SetActive(false);
+        creditos.SetActive(false);
+        salir.SetActive(false);
+    }
+    public void Creditos()
+    {
+        creditos.SetActive(true);
+        inicio.SetActive(false);
+        configuracion.SetActive(false);
+        salir.SetActive(false);
+    }
+    public void Salir()
+    {
+        salir.SetActive(true);
+        creditos.SetActive(false);
+        inicio.SetActive(false);
+        configuracion.SetActive(false);
+    }
+
+    public void Volver()
+    {
+        inicio.SetActive(true);
+        configuracion.SetActive(false);
+        creditos.SetActive(false);
+        salir.SetActive(false);
+    }
+
+    public void Si()
+    {
+        Application.Quit();
+    }
+
+    /*public void Fin()
     {
         if (vida.defeat == false)
         {
             victoria.SetActive(true);
+            gameOver.SetActive(false);
         }
         else
         {
+            victoria.SetActive(false);
             gameOver.SetActive(true);
         }
-    }
-
-
+    }*/
 }
