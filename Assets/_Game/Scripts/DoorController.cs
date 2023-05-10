6bca3eec61f5aa427c75188ff4337c2e62aa66ab
samuelslzrs <Mrsamusasi@gmail.com>
2023-05-09 19:03:44 -0500

    Abrir y cerrar puertas 02 (TERMINADO)
    Melo pa

--- Assets/_Game/Scripts/DoorController.cs
+++ Assets/_Game/Scripts/DoorController.cs
@@ -6,24 +6,21 @@ using UnityEngine.InputSystem;
 public class DoorController : MonoBehaviour
 {
     [SerializeField] private Animator Door = null;
-    [SerializeField] private bool openTrigger = false;
-    [SerializeField] private bool closeTrigger = false;
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            if (openTrigger)
-            {
-                Door.Play("DoorOpen", 0, 0.0f);
-                gameObject.SetActive(false);
-            }
-            else if (closeTrigger)
-            {
-                Door.Play("DoorClose", 0, 0.0f);
-                gameObject.SetActive(false);
-            }
+            Door.Play("DoorOpen", 0, 0.0f);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Door.Play("DoorClose", 0, 0.0f);
+
         }
     }
 }
